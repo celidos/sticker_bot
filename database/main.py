2ab264d4798ab92e7fb3d98dd3d93adec82e5606
Pockonechnyy Eduard <pokonechnyy.ep@phystech.edu>
2022-12-22 14:44:26 +0300

    added postgresql FTS

--- database/main.py
+++ database/main.py
@@ -1,44 +1,58 @@
 import sqlalchemy
-from sqlalchemy import Table, Column, Integer, Text
+from sqlalchemy import Table, Column, Integer, Text #, Model
 import pandas as pd
+import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.types import UserDefinedType
 
-db_engine = sqlalchemy.create_engine('sqlite:////home/celidos/Documents/GIT/sticker_bot/database/db_stickers.sqlite')
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+db = sa.create_engine('postgresql+psycopg2://admin:admin@127.0.0.1:5432/db_stickers')
 # ensure this is the correct path for the sqlite file.
 
-Session = sessionmaker(bind=db_engine)
+base = declarative_base()
+
+
+Session = sessionmaker(db)
 session = Session()
 
-meta = sqlalchemy.MetaData()
+base.metadata.create_all(db)
 
-Stickers = sqlalchemy.Table(
-    'stickers',
-    meta,
-    Column('sticker_id', Integer, primary_key=True),
-    Column('descr', Text),
-    Column('file_id', Text),
-    Column('sticker_set_name', Text),
-    Column('file_unique_id', Text)
-)
+
+
+from sqlalchemy.dialects.postgresql import TSVECTOR
+
+class TSVector(sa.types.TypeDecorator):
+    impl = TSVECTOR
+
+
+class Stickers(base):
+    __tablename__ = "stickers"
+    sticker_id = sa.Column(sa.Integer(), primary_key=True)
+    descr = sa.Column(sa.Text())
+    file_id = sa.Column(sa.Text())
+    sticker_set_name = sa.Column(sa.Text())
+    file_unique_id = sa.Column(sa.Text())
+
+    __ts_vector__ = sa.Column(TSVector(), sa.Computed(
+        "to_tsvector('english', descr)",
+         persisted=True))
+
+    __table_args__ = (sa.Index('ix_stickers___ts_vector__',
+          __ts_vector__, postgresql_using='gin'),)
 
 
 def main():
-    context = type('', (), {})()
+    sts = session.query(Stickers)
+    for st in sts:
+        print(st.descr)
 
-
-
-    sts = Stickers.select()#.filter(Stickers.descr.like(u'%press%'))
-    conn = db_engine.connect()
-    result = conn.execute(sts)
-
-    print('Hello!')
-    for row in result:
-        print(row)
-
-    q_req = session.query(Stickers).filter(Stickers.c.descr.op("MATCH")("press")).all()
-    q_res = conn.execute(q_req)
-    print(q_res)
+    results = session.query(Stickers).filter(Stickers.__ts_vector__.match('die')).all()
+    for st in results:
+        print(st.descr)
 
 
 
